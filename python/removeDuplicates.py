41f05dedf3ce46cedc51c0d990ef3e48a9902b25
novandev <donovan.adams@students.makeschool.com>
2019-04-03 19:46:35 -0700

    a duplicate remmover

--- python/removeDuplicates.py
+++ python/removeDuplicates.py
@@ -1,10 +1,12 @@
 dup_list = [0,0,0,0,1,2,3,4,4,4,5,6,7,7,7,8,9]
 
 def remove_duplicates(nums):
+    """
+    Gives a count of the none duplicated charactors. Runs on (O)n
+    """
     count = 1
     for i in range(1,len(nums)-1):
         if nums[i-1] == nums[i]:
-            print("same")
         else:
             count += 1
     print(count)
